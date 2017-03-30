e7b4323f860a58c79c3b4a89f5a474b5337e16d9
Daniela Morais <dani.m@hotmail.co.uk>
2017-03-30 15:28:31 -0300

    Correção de vimrc e gerar ssh
    Retirar tema monochrome do vimrc e gerar SSH

--- config-minimal.sh
+++ config-minimal.sh
@@ -5,17 +5,37 @@ deb_package=false
 rpm_package=false
 
 function install_vim {
-
-}
-
-function install_zsh {
-
+	sudo apt-get install aptitude 	
+	sudo aptitude install vim, curl, git
+	# Install pathogen
+	mkdir -p ~/.vim/autoload ~/.vim/bundle
+	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+    # Install vim and configure
+	cp vimrc ~/ && cd ~/ && mv vimrc .vimrc
+    chown danielamorais:danielamorais .vimrc
+    cd .vim && mkdir _swap
+    chown danielamorais:danielamorais _swap
+    # Install vim-airline
+    cd bundle/ && git clone https://github.com/bling/vim-airline
 }
 
 function generate_ssh {
-
+    ssh-keygen -t rsa -b 4096 -C "dani.m@hotmail.co.uk"
+    eval "$(ssh-agent -s)"
+    ssh-add ~/.ssh/id_rsa
+    git config --global user.name "Daniela Morais"
+    git config --global user.email dani.m@hotmail.co.uk
 }
- 
+
+function install_zsh {
+    aptitude install zsh
+    sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+    # Install zsh highlighting 
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+    echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+    # Add options in plugins 
+}
+
 if [[ $dist == *"Ubuntu"* ]]; then
 	echo "Unfortunately, it's Ubuntu"
 	deb_package=true
@@ -28,7 +48,6 @@ fi
 
 if [ "$deb_package" = true ]; then 
 	install_vim
-	instal_zsh
-	generate_ssh	
-	echo "It's a .deb"
+    generate_ssh
+    install_zsh
 fi 
---
+++ vimrc
@@ -0,0 +1,47 @@
+execute pathogen#infect()
+set dir=~/.vim/_swap/
+filetype plugin indent on
+" show existing tab with 4 spaces width
+set tabstop=4
+" when indenting with '>', use 4 spaces width
+set shiftwidth=4
+" On pressing tab, insert 4 spaces
+set expandtab
+set smartcase
+syntax on
+set updatetime=200
+set hlsearch
+set incsearch
+set nu
+set cursorline
+set laststatus=2
+hi Search ctermbg=None ctermfg=yellow
+let g:syntastic_mode_map = {
+\ "mode": "passive",
+\ "active_filetypes": [],
+\ "passive_filetypes": []}
+inoremap { {<CR>}<Esc>O
+nnoremap J :s/.*\zs;//<Enter> J<Enter>
+nnoremap <expr> J getline(".")[col("$")-2] == ';' ? ':s/.*\zs;//<Enter>J<Enter>' : 'J<Enter>'
+command SC SyntasticCheck
+hi MatchParen cterm=none ctermbg=darkblue ctermfg=white
+noremap Y y$
+set backspace=indent,eol,start
+set lazyredraw " maybe remove this?
+command WQ wq
+command Wq wq
+command Wqa wqa
+command W w
+command Q q
+command QA qa
+command Qa qa
+set ttyfast
+let mapleader = "\<leader>"
+" FIX WHOLE FILE - FIXES INDENTING, TRAILING SPACE, AND CONVERTS TABS TO
+" SPACES
+nnoremap <Leader>f :retab<CR> mzgg=G'z<CR> :%s/\s\+$//<CR>
+set timeoutlen=1000 ttimeoutlen=0
+noremap <Up> <nop>
+noremap <Down> <nop>
+noremap <Left> <nop>
+noremap <Right> <nop>
